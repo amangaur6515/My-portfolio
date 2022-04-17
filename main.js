f0e489d8092a94a8e89a733ed2bbf567d2a414f1
amangaur6515 <amangaur6515@gmail.com>
2022-04-17 09:48:31 +0530

    My portfolio

---
+++ main.js
@@ -0,0 +1,14 @@
+// scroll TOP Button Events
+const scrollButton = document.querySelector(".scroll-top");
+if(scrollButton){
+    window.addEventListener('scroll', ()=> {
+      if(pageYOffset > (window.innerHeight * 1.2)){
+        scrollButton.style.display="flex";
+      }else{
+        scrollButton.style.display="none";
+      }
+    });
+    scrollButton.addEventListener("click", () => {
+      window.scrollTo(0, 0);
+    });
+  }
